7b11d6ab17348fe18292532bebbcf6ba594a2162
Saket Roy <saket.roy@hotmail.com>
2020-01-20 01:52:15 +0530

    Make tweet from bot

--- twitter-bot.js
+++ twitter-bot.js
@@ -1,4 +1,10 @@
 
-
 let twit = require('twit');
 let config = require('./config.js');
+
+const T = new twit(config);
+
+
+T.post('statuses/update', { status: 'test' }, function(err, data, response) {
+    console.log(data)
+  });
